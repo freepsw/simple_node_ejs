a143404d06eb1e71197b6f5ca702ac833cd48b04
박상원 <freepsw@sk.com>
2016-07-27 13:23:19 +0900

    test commit

--- python/simple_data_generator.py
+++ python/simple_data_generator.py
@@ -26,7 +26,7 @@ def makeMessage():
     dstip_1 = random.randrange(1, 10)
     dstip_2 = random.randrange(1, 10)
     dstip_3 = random.randrange(1, 10)
-    dstip_4 = random.randrange(1, 10)
+    dstip_4 = random.randrange(1, 11)
 
     message = "%d %d.%d.%d.%d %d.%d.%d.%d" %(cnt, srcip_1, srcip_2, srcip_3, srcip_4, dstip_1, dstip_2, dstip_3, dstip_4)
     return message
